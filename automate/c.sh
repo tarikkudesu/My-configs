c37416547020c03014a439ea39d77c2547f5a6a6
tarikku_desu <148579319+tarikkudesu@users.noreply.github.com>
2024-11-23 11:05:22 +0100

    Update c.sh

--- automate/c.sh
+++ automate/c.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cat ~/local/automate/MakefileTools/.0001 | tr -d '\n' > Makefile
-find . -name "*.c" >> Makefile
+find . -name "*.c" | tr '\n' ' ' >> Makefile
 cat ~/local/automate/MakefileTools/.0002c >> Makefile
 printf "%%.o: %%.c " >> Makefile
 find . -name "*.h" | tr '\n' ' ' >> Makefile
