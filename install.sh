05721a06b19ed40c7570344ae608cb38cff93617
tarikku_desu <148579319+tarikkudesu@users.noreply.github.com>
2025-01-29 17:50:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/tarikkudesu/My-configs.git ~/local/tr
 chmod +x $(find ~/local/tr/automate -name "*.sh" | tr '\n' ' ')
-cp zshrc ~/.zshrc
+cp ~/local/tr/zshrc ~/.zshrc
 echo "\n\texecute \"source ~/.zshrc\"\n"
