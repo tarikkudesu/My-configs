7921de118c8738da8a1d43b3ed3399848c2072e7
tarikku_desu <148579319+tarikkudesu@users.noreply.github.com>
2025-02-16 12:30:42 +0100

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -2,14 +2,14 @@
 #Author Tarik AMEHRI
 #42login : tamehri
 
-alias cclean="bash -c \"$(curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/Cleaner_42.sh)\"" # 42 tool
-alias clean="bash -c \"$(curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/linuxCleaner.sh)\"" # 42 tool
+alias cclean='curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/Cleaner_42.sh | bash' # 42 tool
+alias clean='curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/linuxCleaner.sh | bash' # 42 tool
 alias code="open -a Visual\ Studio\ Code" # 42 tool
 alias vsc="~/local/code/code" # 42 tool
 alias usage="du -sh * | sort -rh" # 42 tool
 
 alias codeui="mkdir -p .vscode && curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/settings.json > .vscode/settings.json"
-alias help="sh -c \"$(curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/help.sh)\""
+alias help='curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/help.sh | bash'
 alias cppf="c++ -Wall -Wextra -Werror -std=c++98"
-alias cppm="bash -c \"$(curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/cppm.sh)\""
-alias cm="bash -c \"$(curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/cm.sh)\""
+alias cppm='curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/cppm.sh | bash'
+alias cm='curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/cm.sh | bash'
