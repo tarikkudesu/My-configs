d0fbbd5477896b8f19d581150ec3bf6658097161
Amehri Tarik <amehritarik@gmail.com>
2025-04-30 12:17:37 +0100

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -9,7 +9,7 @@ alias vsc="~/local/code/code" # 42 tool
 alias usage="du -sh * | sort -rh" # 42 tool
 
 alias build-dev-env='sh -c "$(curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/web_dev_env.sh)"'
-alias codeui="mkdir -p .vscode && curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/settings.json > .vscode/settings.json"
+alias codeui="mkdir -p .vscode && curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/settings.json > .vscode/settings.json && curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/.prettierrc > .prettierrc"
 alias help='curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/help.sh | bash'
 alias cppf="c++ -Wall -Wextra -Werror -std=c++98"
 alias cppm='curl -fsSL https://raw.githubusercontent.com/tarikkudesu/My-configs/refs/heads/main/cppm.sh | bash'
